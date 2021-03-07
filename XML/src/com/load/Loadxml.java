8e054f0e4d7c2e0a6bf7ca52b4b05a48a7a64319
vladib6 <vladib6@gmail.com>
2021-03-07 12:43:07 +0200

    initial commit

---
+++ XML/src/com/load/Loadxml.java
@@ -0,0 +1,14 @@
+package com.load;
+
+import com.Engine.MainEngine;
+
+import java.io.FileNotFoundException;
+
+public class Loadxml {
+
+
+    public MainEngine load(String filapath) throws FileNotFoundException {
+
+        return null;
+    }
+}
---
+++ XML/src/com/save/Savexml.java
@@ -0,0 +1,13 @@
+package com.save;
+
+import com.Engine.MainEngine;
+
+import sun.applet.Main;
+
+public class Savexml {
+
+    public void save(MainEngine mainEngine,String filepath){
+
+    }
+
+}
